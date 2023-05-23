1f0b63bb558d156227a0596877d5a30c36ee1ed6
marcel <marcelharder@protonmail.com>
2023-05-23 10:50:50 +0200

    locationURL in production changed

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   apiUrl: 'api/',
   hubUrl: 'hubs/',
-  resetUrl:'https://trac.surgical-outcomes.nl/#/resetpassword',
-  locationUrl: 'https://trac.surgical-outcomes.nl/api/',
+  resetUrl:    'https://trac.surgical-outcomes.nl/#/resetpassword',
+  locationUrl: 'https://trac.surgical-outcomes.nl/#/api/',
 
   defaultLanguage: 'nl'
 };
