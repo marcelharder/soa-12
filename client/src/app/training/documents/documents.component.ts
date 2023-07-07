7c885a0cb03046f9b687900dd0f1c105d61690d2
marcel <marcelharder@protonmail.com>
2023-07-07 10:00:18 +0200

    list of documents works

--- client/src/app/training/documents/documents.component.ts
+++ client/src/app/training/documents/documents.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Document_model } from 'src/app/_models/Document_model';
+import { DocumentService } from 'src/app/_services/document.service';
 
 @Component({
   selector: 'app-documents',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./documents.component.css']
 })
 export class DocumentsComponent implements OnInit {
+@Input() userId:number;
 
-  constructor() { }
+listOfDocuments: Document_model[] = [];
+  
+constructor(private ds: DocumentService) { }
 
   ngOnInit() {
+    // get the list of documents
+    this.ds.getDocuments(this.userId).subscribe((next)=>{
+      
+      this.listOfDocuments = next;
+      
+    
+    
+    
+    
+    
+    })
   }
 
 }
--- client/src/app/training/training/training.component.ts
+++ client/src/app/training/training/training.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { AccountService } from 'src/app/_services/account.service';
 
 @Component({
   selector: 'app-training',
@@ -10,18 +11,24 @@ export class TrainingComponent implements OnInit {
 
   epa = 0;
   doc = 0;
+  courses = 0;
+  cme = 0;
+  currentUserId = 0;
 
-  constructor(private alertify: ToastrService) { }
+  constructor(private alertify: ToastrService, private auth: AccountService) { }
 
   ngOnInit() {
+    this.auth.currentUser$.subscribe((u)=>{this.currentUserId = u.UserId})
   }
 
-  showDocuments(){this.epa = 0;this.doc = 1;}
-  showCourses(){this.alertify.info("Courses are showing now")}
-  showEpa(){this.epa = 1;this.doc = 0;}
-  showCME(){this.alertify.info("CME are showing now")}
+  showDocuments(){this.epa = 0;this.cme = 0; this.courses = 0;this.doc = 1;}
+  showCourses()  {this.epa = 0;this.cme = 0; this.courses = 1;this.doc = 0;}
+  showEpa()      {this.epa = 1;this.cme = 0; this.courses = 0;this.doc = 0;}
+  showCME()      {this.epa = 0;this.cme = 1; this.courses = 0;this.doc = 0;}
 
   showEpaPanel(){if(this.epa === 1){return true;} else {return false;}}
   showDocPanel(){if(this.doc === 1){return true;} else {return false;}}
+  showCoursesPanel(){if(this.courses === 1){return true;} else {return false;}}
+  showCMEPanel(){if(this.cme === 1){return true;} else {return false;}}
 
 }
