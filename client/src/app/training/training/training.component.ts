054485a4a1f762560cf1f7639a4abeb1c6353964
marcel <marcelharder@protonmail.com>
2023-07-21 14:52:46 +0200

    zover klaar

--- client/src/app/training/training/training.component.ts
+++ client/src/app/training/training/training.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { User } from 'src/app/_models/User';
 import { AccountService } from 'src/app/_services/account.service';
+import { UserService } from 'src/app/_services/user.service';
 
 @Component({
   selector: 'app-training',
@@ -10,23 +12,58 @@ import { AccountService } from 'src/app/_services/account.service';
 export class TrainingComponent implements OnInit {
 
   epa = 0;
-  doc = 0;
+  doc = 1;
   courses = 0;
   cme = 0;
   currentUserId = 0;
+  currentCountry = "";
+  currentUser: User = {
+    UserId: 0,
+    Id: 0,
+    hospital_id: 0,
+    password: '',
+    UserName: '',
+    Token: '',
+    roles: [],
+    knownAs: '',
+    age: 0,
+    gender: '',
+    created: undefined,
+    image: '',
+    lastActive: undefined,
+    PhotoUrl: '',
+    city: '',
+    mobile: '',
+    email: '',
+    country: '',
+    worked_in: '',
+    active: false,
+    ltk: false,
+    paidTill: undefined
+  };
 
-  constructor(private alertify: ToastrService, private auth: AccountService) { }
+  constructor(private auth: AccountService, private us: UserService) { }
 
   ngOnInit() {
-    this.auth.currentUser$.subscribe((u)=>{this.currentUserId = u.UserId})
+    this.auth.currentUser$.subscribe((u)=>{
+      
+      this.currentUserId = u.UserId;
+      this.us.getUser(this.currentUserId).subscribe((next)=>{
+       this.currentCountry = next.country;
+      })
+    }
+      
+      );
   }
+
+  showEpaButton(){if(this.currentCountry === "NL")return true; else return false;}
 
   showDocuments(){this.epa = 0;this.cme = 0; this.courses = 0;this.doc = 1;}
   showCourses()  {this.epa = 0;this.cme = 0; this.courses = 1;this.doc = 0;}
   showEpa()      {this.epa = 1;this.cme = 0; this.courses = 0;this.doc = 0;}
   showCME()      {this.epa = 0;this.cme = 1; this.courses = 0;this.doc = 0;}
 
-  showEpaPanel(){if(this.epa === 1){return true;} else {return false;}}
+  showEpaPanel(){if(this.epa === 1){return true;} else {return false;};}
   showDocPanel(){if(this.doc === 1){return true;} else {return false;}}
   showCoursesPanel(){if(this.courses === 1){return true;} else {return false;}}
   showCMEPanel(){if(this.cme === 1){return true;} else {return false;}}
