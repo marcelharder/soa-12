2d9f50d9c2354f7ea9c1b9ace37eb636f52d1f3f
marcel <marcelharder@protonmail.com>
2024-05-06 12:54:23 +0200

    busy with users per hospital

--- client/src/app/users/userlist/userlist.component.ts
+++ client/src/app/users/userlist/userlist.component.ts
@@ -54,7 +54,8 @@ export class UserlistComponent implements OnInit {
       this.countries = next;
     });
     this.loadDrops();
-    this.getUsers();
+    this.selectUserPerHospital("01"); //  get the users from Nederland Catharina ZH because thats the first hospital
+    //this.getUsers();
   }
   loadDrops() {}
 
@@ -92,8 +93,12 @@ export class UserlistComponent implements OnInit {
   showHospitalDrop() { if (this.value === 'User management') { return true } }
   getPosition(ltk: boolean) { if (ltk) { return "Surgeon" } else { return "Resident" } }
 
-  selectUserPerHospital() {
-   this.users = this.allUsers.filter(a => a.hospital_id == this.currentHospital);
+  selectUserPerHospital(id: string) {
+
+   //var currentHospitalNo = this.currentHospital.toString();
+   this.userService.getUsersByHospital(id, 1, 10).subscribe((next)=>{this.users = next.result})
+
+  // this.users = this.allUsers.filter(a => a.hospital_id == this.currentHospital);
    
    var help = this.hospitals.filter(a => a.value == this.currentHospital);
    this.currentHospitalName = help[0].description;
